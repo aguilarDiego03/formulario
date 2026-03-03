1112aa5b03b419680e0c9ae12ad225a97a9baa4b
aguilarDiego03 <23308060610084@cetis61.edu.mx>
2026-03-03 10:30:35 -0500

    Add DatePicker functionality to event registration

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import flet as ft
+import datetime
 
 def main(page: ft.Page):
     page.title = "Registro de Eventos"
+    
+
+def main(page: ft.Page):
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    def handle_change(e: ft.Event[ft.DatePicker]):
+        page.add(ft.Text(f"Date changed: {e.control.value.strftime('%m/%d/%Y')}"))
+
+    def handle_dismissal(e: ft.Event[ft.DialogControl]):
+        page.add(ft.Text("DatePicker dismissed"))
+
+    today = datetime.datetime.now()
+
+    d = ft.DatePicker(
+        first_date=datetime.datetime(year=today.year - 1, month=1, day=1),
+        last_date=datetime.datetime(year=today.year + 1, month=today.month, day=20),
+        on_change=handle_change,
+        on_dismiss=handle_dismissal,
+    )
 
     nombre = ft.TextField(
         label="Nombre del evento",
@@ -45,8 +65,19 @@ def main(page: ft.Page):
         weight=ft.FontWeight.BOLD
     )
 
+    def mostrar(e):
+        resumen.value = (
+            "Nombre: " + nombre.value + "\n"
+            "Tipo: " + tipo.value + "\n"
+            "Modalidad: " + modalidad.value + "\n"
+            "Inscripción previa: " + ("Sí" if inscripcion.value else "No") + "\n"
+            "Duración: " + str(int(duracion.value)) + " horas"
+        )
+        page.update()
+
     boton = ft.ElevatedButton(
         "Mostrar resumen",
+        on_click=mostrar
     )
 
     page.add(
@@ -58,7 +89,12 @@ def main(page: ft.Page):
         duracion,
         boton,
         ft.Divider(),
-        resumen
+        resumen,
+        ft.Button(
+            content="Pick date",
+            icon=ft.Icons.CALENDAR_MONTH,
+            on_click=lambda e: page.show_dialog(d),
+        )
     )
 
 ft.app(target=main)
